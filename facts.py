30c12a575ee7978b7168d114ff82a02297f70866
jordan <jordan@jordanmartel.com>
2021-08-08 23:41:44 -0400

    ENH: plot sketch

---
+++ facts.py
@@ -0,0 +1,93 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import cleaning
+import portfolio
+import twostock
+import beta_plots
+import portfolio
+
+font = {'family' : 'normal', 'size' : 12}
+
+# parameters
+digits = 4
+
+# read CRSP data
+D = pd.read_csv('dow30_data.csv')
+
+# renames
+D = D.rename(columns={'TICKER' : 'ticker'})
+
+# clean-up
+D = D[D.ticker!='C']                        # C not in the DIJA
+D = D[D.RET!='C']							# V first obs
+D.loc[D.ticker=='WAG','ticker'] = 'WBA' 	# WBA ticker change
+
+# sort
+D.date = D.date.map(lambda d: int(str(d)[0:6]))
+
+# names of the stocks
+N = pd.read_csv('dow30_name.csv')
+
+# load Fama-French data
+M = pd.read_csv('famafrench.csv')
+
+# merge
+D = D.merge(N) 
+D = D.merge(M)
+
+# change date format
+D.date = pd.to_datetime(D.date,format='%Y%m')
+M.date = pd.to_datetime(M.date,format='%Y%m')
+
+D['ret'] = np.double(D.RET)
+D['rf'] = D.rf
+
+# round 
+D = cleaning.round_columns(D) 
+
+# build portfolio
+portfolio1 = twostock.TwoStocks(D,'AAPL','PG',n=1000)
+portfolio2 = twostock.TwoStocks(D,'CVX','XOM',n=1000)
+print(portfolio2.summary())
+#portfolio2 = portfolio.Portfolio(D,['AAPL','PG'],.5])
+
+# load wide 
+W = pd.read_csv('canvas_wide.csv')
+
+# betas
+stats, beta, sharpe = beta_plots.beta_plots(D.loc[D['ticker']=='CAT',['mkt','ret','rf']])
+
+bigport = portfolio.Portfolio(D[['ticker','date','ret']])
+
+# histogram
+if False:
+	X = W.corr().to_numpy()
+	X = np.triu(X,1)
+	X = X[X.nonzero()]
+	plt.hist(X,density=True,rwidth=.9)
+	plt.xlim([0.,1.])
+	plt.xticks(**font)
+	plt.yticks(**font)
+	plt.xlabel('Correlation',**font)
+	plt.ylabel('Frequency',**font)
+	plt.title('Correlation Between DIJA 30 Stocks',**font)
+	plt.show()
+	print(X.mean())
+
+# value of diversification
+if False:
+	X = W.cov().to_numpy()
+	portvar = []
+	N = 30
+	for n in range(1,N+1):
+		x = np.hstack([np.ones(n)/n,np.zeros(N-n)])
+		portvar.append(100.*np.sqrt(X.dot(x).dot(x)))
+	portvar = np.array(portvar)
+	plt.plot(np.arange(1,N+1),portvar,linewidth=4)
+	plt.xticks(**font)
+	plt.yticks(**font)
+	plt.xlabel('Number of Stocks',**font)
+	plt.ylabel('Portfolio Standard Deviation (%)',**font)
+	plt.title('Equally-Weighted Portfolio Risk (Adding Alphabetically)',**font)
+	plt.show()
--- portfolio.py
+++ portfolio.py
@@ -8,6 +8,22 @@ from alpha_vantage.timeseries import TimeSeries
 
 font = {'family' : 'normal', 'size' : 12}
 
+# efficient frontier
+@np.vectorize
+def _ef(n,mu):
+    """
+    Efficient frontier
+    
+    Parameters
+    ----------
+    mu_port : float
+        Desired expected return
+    """
+    rhs = np.block([self.o.T,mu_port,1.]).T
+    x = lu_solve((self.lu,self.piv),rhs)[:-2]
+    s = x.T@(self.Sigma@x)
+    return np.sqrt(s[0])
+
 class Portfolio:
 
     def __init__(self,key,tickers,rF=0.,hor=[1,5]):
@@ -30,7 +46,7 @@ class Portfolio:
 
         Examples
         --------
-        """	 
+        """     
 
         # rates
         self.rF = rF
@@ -71,7 +87,7 @@ class Portfolio:
                 X = X.merge(tick_dat,**opts)
 
         # drop
-        X.to_csv('_'.join(tickers) + '.csv')		
+        X.to_csv('_'.join(tickers) + '.csv')        
 
         # compute returns 
         for t in tickers:
@@ -88,111 +104,144 @@ class Portfolio:
         # column names
         self.columns = [ticker + '_RET' for ticker in self.tickers]
 
-	def summary(self,h=60):
+    def summary(self,h=60,to_csv=False):
+        """
+        Parameters
+        ----------
+        m : int
+            Month
+        h : int
+            Horizon (in years) over which to compute stats
+
+        Returns
+        -------
+        _stat : pandas.DataFrame
+            DataFrame indexed by tickers containing various statistics
+        _corr :
+            Correlation table 
+        """
+
+        # -------------------------------------------------------------------
+        # PORTFOLIO STATISTICS
+
+        # subset
+        idx = self.X.index[-h-2:-2]
+        _X = self.X.loc[idx]
+
+        # drop
+        if to_csv:
+            _X.to_csv('_'.join(self.tickers) + '_JORDAN.csv')        
+
+        # statistics
+        self.mu = np.matrix(_X[self.columns].mean().to_numpy()).T
+        self.Sigma = _X[self.columns].cov().to_numpy()
+
+        # TODO: use .cov()
+        _corr = _X[self.columns].corr()
+        _corr.index = self.tickers
+        
+        # pre-processing for efficient frontier
+        self.n = len(self.tickers)
+        self.z = np.zeros((self.n,1))
+        self.o = np.ones((self.n,1))
+
+        # matrix and its lu decomposition
+        self.lu, self.piv = lu_factor(self.Sigma)
+
+        def _apply(v,s):
             """
+            Helper function 
+
             Parameters
             ----------
-            m : int
-                Month
-            h : int
-                Horizon (in years) over which to compute stats
-
-            Returns
-            -------
-            _stat : pandas.DataFrame
-                DataFrame indexed by tickers containing various statistics
-            _corr :
-                Correlation table 
+            v : str
+                Variable (e.g. '_RET')
+            s : str
+                Statistic; must be "applicable" using pandas.DataFrame.apply
             """
+            cols = [ticker + v for ticker in self.tickers]
+            stat = _X[cols].apply(s)
+            stat.name = s # TODO: figure out the correct way of doing this
+            stat.index = self.tickers
+            return stat
 
-            # -------------------------------------------------------------------
-            # PORTFOLIO STATISTICS
+        # compute statistics
+        _stat = {}
+        _stat['sys'] = _corr['SPY_RET']        # %systematic
+        _stat['exp'] = _apply('_RET','mean')   # expected return
+        _stat['vol'] = _apply('_RET','std')    # volatility
+        _stat['div'] = _apply('_DY','mean')    # dividend yield
+        _stat['cap'] = _apply('_CG','mean')    # capital gain rate
 
-            # subset
-            idx = self.X.index[-h-2:-2]
-            _X = self.X.loc[idx]
+        _stat = pd.DataFrame(_stat)
 
-            # drop
-            _X.to_csv('_'.join(self.tickers) + '_JORDAN.csv')		
+        # auxiliary
+        _mvol = _stat.loc['SPY','vol']                      # market volatility
+        _stat['idi'] = 1.-_stat['sys']                      # %idiosyncratic 
+        _stat['bet'] = (_stat['vol']/_mvol)*_stat['sys']    # beta
 
-            # statistics
-            self.mu = np.matrix(_X[self.columns].mean().to_numpy()).T
-            self.Sigma = _X[self.columns].cov().to_numpy()
+        # alpha
+        lm = self.X.index[-2]
+        t1 = self.X.loc[lm,self.columns]
+        t1.index = self.tickers
+        t2 = _stat['bet']*self.X.loc[lm,'SPY_RET'] 
+        _stat['alp'] = t1-t2
 
-            # TODO: use .cov()
-            _corr = _X[self.columns].corr()
-            _corr.index = self.tickers
-            
-            # pre-processing for efficient frontier
-            self.n = len(self.tickers)
-            self.z = np.zeros((self.n,1))
-            self.o = np.ones((self.n,1))
+        # drop it
+        return _stat, _corr
 
-            # matrix and its lu decomposition
-            A = np.block([
-                [self.Sigma,self.mu,self.o],
-                [self.mu.transpose(),0.,0.],
-                [self.o.transpose(),0.,0.]])
-            self.lu, self.piv = lu_factor(A)
+    def plot(self,n_plot=100,cml=True,cal=None,risk=None,ef=True):
+        """
+        Plots risk-return space
+        
+        Parameters
+        ----------
+        cml : boolean
+            If True, draws the Capital Market Line (CML)
+        cal : float
+            If not None, draws a Capital Allocation Line (CAL) with mean <cal>
+        risks : str
+            If not None, delineates the systematic and idiosyncratic risks
+        ef : boolean
+            If True, draws the efficient frontier
+        """
+        mu_plt = np.linspace(-1,2,n_plt) 
 
-            # efficient frontier
-            @np.vectorize
-            def _ef(mu_port):
-                """
-                Efficient frontier
-                
-                Parameters
-                ----------
-                mu_port : float
-                        Desired expected return
-                """
-                rhs = np.block([self.o.T,mu_port,1.]).T
-                x = lu_solve((self.lu,self.piv),rhs)[:-2]
-                s = x.T@(self.Sigma@x)
-                return np.sqrt(s[0])
+        # efficient frontier
+        if ef:
+            plt.plot(_ef(mu_plt,self.lu,self.piv),mu_plt)
 
-            def _apply(v,s):
-                """
-                Helper function 
+        # tickers to plot
+        if ticks:
+            for ticker in tickers:
+                ticker_mean = P.D[ticker].mean()
+                ticker_stdv = P.D[ticker].std()
+                plt.annotate(ticker,(ticker_stdv,ticker_mean))
+                plt.plot(ticker_stdv,ticker_mean,'ok')	
 
-                Parameters
-                ----------
-                v : str
-                    Variable (e.g. '_RET')
-                s : str
-                    Statistic; must be "applicable" using pandas.DataFrame.apply
-                """
-                cols = [ticker + v for ticker in self.tickers]
-                stat = _X[cols].apply(s)
-                stat.name = s # TODO: figure out the correct way of doing this
-                stat.index = self.tickers
-                return stat
+        # draws a capital allocation line
+        def cal(weights):
+            t = np.linspace(-.25,2.25,n_plot)
+            plt.plot(t*P.stdv_p,t*P.mean_p)
+            plt.plot(P.stdv_p,P.mean_p,'ok')
+            plt.annotate('Portfolio',(P.stdv_p,P.mean_p))
 
-		# compute statistics
-		_stat = {}
-		_stat['sys'] = _corr['SPY_RET']		# %systematic
-		_stat['exp'] = _apply('_RET','mean')    # expected return
-		_stat['vol'] = _apply('_RET','std')	# volatility
-		_stat['div'] = _apply('_DY','mean')	# dividend yield
-		_stat['cap'] = _apply('_CG','mean')	# capital gain rate
+        # capital allocation line for portfolio 'x'
+        if cal: 
+            cal(x)
 
-		_stat = pd.DataFrame(_stat)
+        # capital market line
+        if cml:
+            cal(x_tan)
 
-		# auxiliary
-		_mvol = _stat.loc['SPY','vol']		# market volatility
-		_stat['idi'] = 1.-_stat['sys']		# %idiosyncratic 
-		_stat['bet'] = (_stat['vol']/_mvol)*_stat['sys']	 # beta
+        # plot the portfolio
+        if port:
+            plt.plot(stdv_p,mean_p,'-')
+        plt.plot(0.,0.,'ok')
+        plt.annotate('T-bill',(0.,0.))
 
-		# alpha
-		lm = self.X.index[-2]
-		t1 = self.X.loc[lm,self.columns]
-		t1.index = self.tickers
-		t2 = _stat['bet']*self.X.loc[lm,'SPY_RET'] 
-		_stat['alp'] = t1-t2
-
-		# drop it
-		return _stat, _corr
-
-	def __str__(self):
-            """prints statistics about the portfolio"""
-	    pass
+        # cosmetics
+        plt.xlabel('Monthly Risk (%)')
+        plt.ylabel('Monthly Return (%)')
+        plt.grid()
+        plt.show()
